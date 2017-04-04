06280f18d22140c42f1a1c601f2fe078703f80c3
Justin Geiger <ablecken@gmail.com>
2017-04-04 10:08:31 -0500

    fix some from accounts for email

--- infrastructure/emailer.js
+++ infrastructure/emailer.js
@@ -2,7 +2,7 @@ const app = require('../app');
 const mailer = require('express-mailer');
 
 mailer.extend(app, {
-	from: 'ablecken@gmail.com',
+	from: 'jgeiger@sparzington.com',
 	host: 'localhost',
 	port: 25,
 	transportMethod: 'SMTP'
