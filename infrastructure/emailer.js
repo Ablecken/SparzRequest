9dfda576a0b401f04d588309f570e32ff0dcd004
Justin Geiger <ablecken@gmail.com>
2017-04-04 10:12:10 -0500

    use the right email!

--- infrastructure/emailer.js
+++ infrastructure/emailer.js
@@ -2,7 +2,7 @@ const app = require('../app');
 const mailer = require('express-mailer');
 
 mailer.extend(app, {
-	from: 'jgeiger@sparzington.com',
+	from: 'justin@sparzington.com',
 	host: 'localhost',
 	port: 25,
 	transportMethod: 'SMTP'
